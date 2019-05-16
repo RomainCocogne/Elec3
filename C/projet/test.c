27fdd757eced0b7fd3c2d908326cc41c3d635c3a
Romain <roco2904@gmail.com>
2019-05-16 19:23:29 +0200

    update

--- C/projet/test.c
+++ C/projet/test.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include "carte.h"
 
+<<<<<<< Updated upstream
 #define DEFAULT_CARD_WIDTH 100
 #define DEFAULT_CARD_HEIGHT 200
 
@@ -12,11 +13,50 @@ int gris;
 int blanc;
 int card_width;
 int card_height; 
+=======
+// #define CARD_WIDTH 50
+// #define CARD_HEIGHT 100
+
+// //Pour la couleur des cartes
+// int gris;
+// int blanc; 
+
+// // void redisplay(Widget w, int width, int height, void *data){
+// //     DrawImage((char*)data,0,0,iWidth,iHeight);
+// // }
+
+// void retournerCarte(Widget w, int which_button, int x, int y, void *data){
+//     SetDrawArea(w);
+//     int mode = getCardMode(data);
+//     if (mode == CACHEE){
+//         char str[3];
+//         sprintf(str,"%d",getCardId(data));
+//         printf("%s\n",str );
+//         SetColor(blanc);
+//         SetBgColor(w,gris);
+//         DrawText(str,CARD_WIDTH/2,CARD_HEIGHT/2);
+//         setCardMode(data,RETOURNEE);
+//     }
+//     else{
+//         SetColor(gris); 
+//         DrawFilledBox(0,0,CARD_WIDTH,CARD_HEIGHT);
+//         setCardMode(data,CACHEE);
+//     }
+
+// }
+
+// void initJeuCartes(Card *TabCartes,int nbCartes){
+//     for (int k=0; k<nbCartes; k++){
+//         initCard(TabCartes+k,k,CACHEE);
+//     }
+// }
+>>>>>>> Stashed changes
 
 // void initDrawArea(Widget w, int width, int height, void *data){
 //     SetColor(gris);
 //     // SetWidgetFont(w,GetFont("comicSansMS"));
 //     DrawFilledBox(0,0,CARD_WIDTH,CARD_HEIGHT);
+<<<<<<< Updated upstream
 // void redisplay(Widget w, int width, int height, void *data){
 //     DrawImage((char*)data,0,0,iWidth,iHeight);
 // }
@@ -94,6 +134,45 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Can't open display\n");
 		return EXIT_FAILURE;
 	}
+=======
+// }
+
+// void initAffichage(Card *tabCartes, int grilleWidth, int grilleHeight){
+//     Widget tabWidget[grilleHeight*grilleWidth];
+
+//     for (int k=0; k<grilleHeight*grilleWidth;k++){
+//         tabWidget[k] = MakeDrawArea(CARD_WIDTH,CARD_HEIGHT,initDrawArea,tabCartes+k);
+//         SetButtonDownCB(tabWidget[k],retournerCarte);
+//         // initDrawArea(tabWidget[k]);
+//     }
+
+//     Widget yposWidget = NULL;
+//     Widget xposWidget = NULL;
+
+//     int i,j, right=NO_CARE, under=NO_CARE;
+//     for (i = 0; i < grilleHeight; ++i)
+//     {
+
+//         for (j = 0; j < grilleWidth; ++j)
+//         {
+//             SetWidgetPos(tabWidget[i*grilleWidth + j], right, xposWidget, under, yposWidget);
+//             xposWidget = tabWidget[i*grilleWidth + j];
+//             right = PLACE_RIGHT;
+//         }
+//         if (i*grilleWidth+j < grilleHeight*grilleWidth) yposWidget = tabWidget[i*grilleWidth];
+//         right = NO_CARE;
+//         under = PLACE_UNDER; 
+//     }
+// }
+
+// int main(int argc, char *argv[])
+// {
+
+// 	if (OpenDisplay(argc, argv)==0){
+// 		fprintf(stderr, "Can't open display\n");
+// 		return EXIT_FAILURE;
+// 	}
+>>>>>>> Stashed changes
     
 //     int grilleWidth = 7;
 //     int grilleHeight = 4;
@@ -102,10 +181,17 @@ int main(int argc, char *argv[])
 //     Card tabCartes[size];
 //     initJeuCartes(tabCartes,size);
 
+<<<<<<< Updated upstream
 	GetStandardColors();
     gris = GetRGBColor(20,20,20);
     blanc = GetRGBColor(250,250,250);
     initAffichage(tabCartes, grilleWidth, grilleHeight);
+=======
+// 	GetStandardColors();
+//     gris = GetRGBColor(20,20,20);
+//     blanc = GetRGBColor(230,230,230);
+//     initAffichage(tabCartes, grilleWidth, grilleHeight);
+>>>>>>> Stashed changes
     
 // 	ShowDisplay();
 // 	MainLoop();
