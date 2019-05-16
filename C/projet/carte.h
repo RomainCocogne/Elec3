ee56d062111d89cff48024d40714f7be9341ecf1
Quentin <quentin.combal@hotmail.fr>
2019-05-16 16:45:14 +0200

    update graphique projet c

--- C/projet/carte.h
+++ C/projet/carte.h
@@ -1,21 +1,28 @@
 #pragma once 
 
-enum FORME {CARRE, ROND, TRIANGLE,ETOILE};
+#define CACHEE		0
+#define RETOURNEE	1
 
 typedef struct
 {
-	int mode;
-	enum FORME f;
+	int id,mode;
 
 } Card;
 
-extern int getCardForme(Card c);
+extern int getCardId(Card *cardPointer);
 
-extern int getCardMode(Card c);
+extern int getCardMode(Card *cardPointer);
 
+// extern int getCardX(Card c);
+
+// extern int getCardY(Card c);
 
 extern void setCardMode(Card *cardPointer, int newMode);
-void setCardForme(Card *cardPointer, int newforme);
 
-extern void initCard(Card *carte,int f, int mode);
+// extern void setCardX(Card *cardPointer, int newX);
 
+// extern void setCardY(Card *cardPointer, int newY);
+
+extern void initCard(Card *cartep, int id, int mode);
+
+ 
