caf4354dbe9f6b5928ed9826873125f8218fce67
Romain <roco2904@gmail.com>
2019-06-05 14:04:24 +0200

    rm shuffle

--- C/projet/shuffle.c
+++
@@ -1,8 +0,0 @@
-#include "shuffle.h"
-
-/* arrange the N elements of ARRAY in random order.
- * Only effective if N is much smaller than RAND_MAX;
- * if this may not be the case, use a better random
- * number generator. */
-
-
