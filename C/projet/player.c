2df1ba00feb3afdb933535706d23d85abecedecf
Romain <roco2904@gmail.com>
2019-06-09 14:16:08 +0200

    finish sur player

--- C/projet/player.c
+++ C/projet/player.c
@@ -1,33 +1,71 @@
 /*
-
+	Implementation des fonctions de player.h
 */
 #include "player.h"
 
+/*
+	Compare les noms et scores de 2 joueurs.
+	@args:
+		- j1: Player premier joueur a comparer
+		- j2: Player second joueur a comparer
+	@return:
+		- int resultat de la comparaison. 0 si les noms sont differents et scores egaux,
+			  sinon, -1 si score du premier joueur et plus grand, 1 sinon
+*/
 int compare (Player j1, Player j2){
 	if (!strcmp(j1.name,j2.name) && j1.score==j2.score)
 		return 0;
 	return j1.score>j2.score? -1:1;
 }
 
+/*
+	Initialise la structure Player
+	@args:
+		- j: Pointeur sur le Player a Initialiser
+*/
 void initPlayer(Player *j){
 	j->name=malloc(MAX_NAME*sizeof(char));
 	j->score=0;
 }
 
+/*
+	Supprime un joueur
+	@args:
+		- j: Pointeur sur le Player a supprimer
+*/
 void rmPlayer(Player *j){
+	free(j->name);
 	free(j);
 }
 
+/*
+	Permet de donner un nom au joueur.
+	Si un nom est deja donne, il sera ecrase
+	@args:
+		- j: Pointeur sur le Player dont on veut modifier le nom
+		- name: Pointeur sur char (chaine de caracteres) representant le nom
+*/
 void setPlayerName(Player *j, char *name){
 	strcpy(j->name,name);
 }
 
+/*
+	Permet de donner un score a un joueur.
+	@args:
+		- j: Pointeur sur le Player dont on veut modifier le score
+		- score: int le score
+*/
 void setPlayerScore(Player *j, int score){
 	j->score=score;
 }
 
+/*
+	Copie le nom et le score d'un joueur dans un autre joueur
+	@args:
+		- j1: Pointeur sur le joueur recepteur
+		- j2: Pointeur sur le joueur donneur
+*/
 void playercpy(Player *j1, Player *j2){
-	initPlayer(j1);
 	strcpy(j1->name,j2->name);
 	j1->score=j2->score;
 }
--- C/projet/player.h
+++ C/projet/player.h
@@ -2,20 +2,81 @@
 	header du module de la gestion de joueurs
 */
 #pragma once
+
+/****************************/
+//		 inclusions 		//
+/****************************/
 #include <string.h> 
 #include "liste.h"
 
+/****************************/
+//			 macros 		//
+/****************************/
+#define MAX_NAME 100
+
+/****************************/
+//			 types	 		//
+/****************************/
+/*
+	structure de joueur contenant le nom et le score du joueur
+*/
 typedef struct{
 	char *name;
 	int score;
 } Player;
 
 
-#define MAX_NAME 100
 
-extern int compare(Player j1, Player j2);
-extern void initPlayer(Player *j);
-extern void rmPlayer(Player *j);
-extern void setPlayerName(Player *j, char *name);
-extern void setPlayerScore(Player *j, int score);
-extern void playercpy(Player *j1, Player *j2);
+/****************************/
+//	    	fonctions 		//
+/****************************/
+
+/*
+	Compare les noms et scores de 2 joueurs.
+	@args:
+		- Player premier joueur a comparer
+		- Player second joueur a comparer
+	@return:
+		- int resultat de la comparaison. 0 si les noms sont differents et scores egaux,
+			  sinon, -1 si score du premier joueur et plus grand, 1 sinon
+*/
+extern int compare(Player, Player);
+
+/*
+	Initialise la structure Player
+	@args:
+		- Pointeur sur le Player a Initialiser
+*/
+extern void initPlayer(Player *);
+
+/*
+	Supprime un joueur
+	@args:
+		- Pointeur sur le Player a supprimer
+*/
+extern void rmPlayer(Player *);
+
+/*
+	Permet de donner un nom au joueur.
+	Si un nom est deja donne, il sera ecrase
+	@args:
+		- Pointeur sur le Player dont on veut modifier le nom
+		- Pointeur sur char (chaine de caracteres) representant le nom
+*/
+extern void setPlayerName(Player *, char *);
+
+/*
+	Permet de donner un score a un joueur.
+	@args:
+		- Pointeur sur le Player dont on veut modifier le score
+		- int le score
+*/
+extern void setPlayerScore(Player *, int);
+
+/*
+	Copie le nom et le score d'un joueur dans un autre joueur
+	@args:
+		- Pointeur sur le joueur recepteur
+		- Pointeur sur le joueur donneur
+*/
+extern void playercpy(Player *, Player *);
